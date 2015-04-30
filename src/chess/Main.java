11a8e22f71a9c4e42346994652ac5aaeaae1b1c1
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2015-04-30 16:38:27 -0300

    Init project

---
+++ src/chess/Main.java
@@ -0,0 +1,12 @@
+package chess;
+import view.BasicChessView;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		BasicChessView chessView = new BasicChessView();
+		chessView.setVisible(true);
+	}
+
+}
