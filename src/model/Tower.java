ef46306d76f67c4dc9d658618b455ac9a0ddf347
Luan Guimarães <luan@live.in>
2015-05-21 15:57:27 -0300

    Adding Tower class that extends Piece class.

---
+++ src/model/Tower.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Tower extends Piece {
+	
+	private boolean mooved;
+	
+	public Tower (String imagePath){
+		super(imagePath);
+		this.mooved = false;
+	}
+	
+	public Tower (String imagePath, Point position){
+		super(imagePath, position);
+		this.mooved = false;
+	}
+	
+	@Override
+	public ArrayList<Point> getPossiblePoints(Point myPoint){	
+		return null;
+	}
+	
+	public boolean alreadyMooved(){
+		return mooved;
+	}
+}
