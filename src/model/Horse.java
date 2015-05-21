33d1093f78f97808492f0fd8887837a989256960
Luan Guimarães <luan@live.in>
2015-05-21 15:55:47 -0300

    Adding Horse class that extends Piece class.

---
+++ src/model/Horse.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Horse extends Piece {
+	
+	public Horse (String imagePath){
+		super(imagePath);
+	}
+	
+	public Horse (String imagePath, Point position){
+		super(imagePath, position);
+	}
+	
+	@Override
+	public ArrayList<Point> getPossiblePoints(Point myPoint){	
+		return null;
+	}
+}
