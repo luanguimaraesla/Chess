04550c639bb94e9cc546dde8448fb0766c0151d3
Luan Guimarães <luan@live.in>
2015-05-21 15:56:30 -0300

    Adding Queen class that extends Piece class.

---
+++ src/model/Queen.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Queen extends Piece {
+	
+	public Queen (String imagePath){
+		super(imagePath);
+	}
+	
+	public Queen (String imagePath, Point position){
+		super(imagePath, position);
+	}
+	
+	@Override
+	public ArrayList<Point> getPossiblePoints(Point myPoint){	
+		return null;
+	}
+}
