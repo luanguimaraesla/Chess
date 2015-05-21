ca740638e90c6e423f3d22c351fcaad8cf517905
Luan Guimarães <luan@live.in>
2015-05-20 23:32:57 -0300

    Adding class Piece.

---
+++ src/model/Piece.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Piece {
+	public static final char PAWN = 'P';
+	public static final char TOWER = 'T';
+	public static final char HORSE = 'H';
+	public static final char BISHOP = 'B';
+	public static final char QUEEN = 'Q';
+	public static final char KING = 'K';
+	
+	private char id;
+	private String imagePath;
+	
+	public Piece (char id, String imagePath){
+		this.id = id;
+		this.imagePath = imagePath;
+	}
+	
+	public char getId() {
+		return id;
+	}
+	public void setId(char id) {
+		this.id = id;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+}
