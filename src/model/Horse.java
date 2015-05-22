fe074d9aea3ac65aabbc3efcfc179e6f0e255e7a
Luan Guimarães <luan@live.in>
2015-05-22 17:01:58 -0300

    Adding method to recover all the possible available points for Horse piece.

--- src/model/Horse.java
+++ src/model/Horse.java
@@ -15,6 +15,15 @@ public class Horse extends Piece {
 	
 	@Override
 	public ArrayList<Point> getPossiblePoints(){	
-		return null;
+		ArrayList<Point> possiblePointsToGo = new ArrayList<Point>();
+		int x = (int)this.getPosition().getX();
+		int y = (int)this.getPosition().getY();
+		
+		for(int i = -2; i <= 2; i++)
+			for(int j = -2; j <= 2; j++)
+				if(Math.abs(i) != Math.abs(j) && i != 0 && j != 0)
+					possiblePointsToGo.add(new Point(i + x, j + y));
+		
+		return possiblePointsToGo;
 	}
 }
