e2616216fe06d9941683e3d7588871749516510e
Luan Guimarães <luan@live.in>
2015-05-21 23:00:28 -0300

    Redefining method 'initializePiecesInChess()' to set Square pieces from already created each Team pieces.

--- src/view/SquareBoardPanel.java
+++ src/view/SquareBoardPanel.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 import model.Piece;
 import model.Square;
 import control.SquareControl;
+import control.GameControl;
 
 public class SquareBoardPanel extends JPanel {
 
@@ -17,11 +18,13 @@ public class SquareBoardPanel extends JPanel {
 
 	private SquareControl squareControl;
 	private ArrayList<SquarePanel> squarePanelList;
+	private GameControl gameControl;
 
 	public SquareBoardPanel() {
 		setLayout(new GridBagLayout());
 		this.squarePanelList = new ArrayList<SquarePanel>();
-
+		this.gameControl = new GameControl();
+		
 		initializeSquareControl();
 		initializeGrid();
 		initializePiecesInChess();
@@ -55,51 +58,15 @@ public class SquareBoardPanel extends JPanel {
 	}
 
 	private void initializePiecesInChess() {
-		String piecePath = "icon/Brown P_48x48.png";
-		for (int i = 0; i < SquareControl.COL_NUMBER; i++) {
-			this.squareControl.getSquare(1, i).setPiece(new Piece(Piece.PAWN, piecePath));
+		for (Piece eachPiece : gameControl.getBrownTeam()) {
+			this.squareControl.getSquare((int)(eachPiece.getPosition().getX()),
+										(int)(eachPiece.getPosition().getY())).setPiece(eachPiece);
 		}
 		
-		piecePath = "icon/Brown R_48x48.png";
-		this.squareControl.getSquare(0, 0).setPiece(new Piece(Piece.TOWER, piecePath));
-		this.squareControl.getSquare(0, 7).setPiece(new Piece(Piece.TOWER, piecePath));
-
-		piecePath = "icon/Brown N_48x48.png";
-		this.squareControl.getSquare(0, 1).setPiece(new Piece(Piece.HORSE, piecePath));
-		this.squareControl.getSquare(0, 6).setPiece(new Piece(Piece.HORSE, piecePath));
-
-		piecePath = "icon/Brown B_48x48.png";
-		this.squareControl.getSquare(0, 2).setPiece(new Piece(Piece.BISHOP, piecePath));
-		this.squareControl.getSquare(0, 5).setPiece(new Piece(Piece.BISHOP, piecePath));
-
-		piecePath = "icon/Brown Q_48x48.png";
-		this.squareControl.getSquare(0, 4).setPiece(new Piece(Piece.QUEEN, piecePath));
-
-		piecePath = "icon/Brown K_48x48.png";
-		this.squareControl.getSquare(0, 3).setPiece(new Piece(Piece.KING, piecePath));
-
-		
-		piecePath = "icon/White P_48x48.png";
-		for (int i = 0; i < SquareControl.COL_NUMBER; i++) {
-			this.squareControl.getSquare(6, i).setPiece(new Piece(Piece.PAWN, piecePath));
+		for (Piece eachPiece : gameControl.getWhiteTeam()) {
+			this.squareControl.getSquare((int)(eachPiece.getPosition().getX()),
+										(int)(eachPiece.getPosition().getY())).setPiece(eachPiece);
 		}
 		
-		piecePath = "icon/White R_48x48.png";
-		this.squareControl.getSquare(7, 0).setPiece(new Piece(Piece.TOWER, piecePath));
-		this.squareControl.getSquare(7, 7).setPiece(new Piece(Piece.TOWER, piecePath));
-
-		piecePath = "icon/White N_48x48.png";
-		this.squareControl.getSquare(7, 1).setPiece(new Piece(Piece.HORSE, piecePath));;
-		this.squareControl.getSquare(7, 6).setPiece(new Piece(Piece.HORSE, piecePath));
-
-		piecePath = "icon/White B_48x48.png";
-		this.squareControl.getSquare(7, 2).setPiece(new Piece(Piece.BISHOP, piecePath));
-		this.squareControl.getSquare(7, 5).setPiece(new Piece(Piece.BISHOP, piecePath));
-
-		piecePath = "icon/White Q_48x48.png";
-		this.squareControl.getSquare(7, 4).setPiece(new Piece(Piece.QUEEN, piecePath));
-
-		piecePath = "icon/White K_48x48.png";
-		this.squareControl.getSquare(7, 3).setPiece(new Piece(Piece.KING, piecePath));
 	}
 }
