0ce7e12bf92e6a075182d2cc8846f4696331e40d
Luan Guimarães <luan@live.in>
2015-05-24 16:30:40 -0300

    Adding method that returns an ArrayList with all the possible positions Bishop pieces can goes.

--- src/model/Bishop.java
+++ src/model/Bishop.java
@@ -14,7 +14,22 @@ public class Bishop extends Piece {
 	}
 	
 	@Override
-	public ArrayList<Point> getPossiblePoints(){	
-		return null;
+	public ArrayList<Point> getPossiblePoints(){
+		ArrayList<Point> possiblePointsToGo = new ArrayList<Point>();
+		int x = (int)this.getPosition().getX();
+		int y = (int)this.getPosition().getY();
+		
+		for(int i = 1; i < 8; i++){
+			if(x + i < 8 && y + i < 8)
+				possiblePointsToGo.add(new Point(x + i, y + i));
+			if(x - i >= 0 && y + i < 8)
+				possiblePointsToGo.add(new Point(x - i, y + i));
+			if(x + i < 8 && y - i >= 0)
+				possiblePointsToGo.add(new Point(x + i, y - i));
+			if(x - i >= 0 && y - i >= 0)
+				possiblePointsToGo.add(new Point(x - i, y - i));
+		}
+		
+		return possiblePointsToGo;
 	}
 }
