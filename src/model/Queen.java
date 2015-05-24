9dccffe3724893568d400647d51bd2c26f267da6
Luan Guimarães <luan@live.in>
2015-05-24 20:00:21 -0300

    Adding method that returns an ArrayList with all the possible positions Queen pieces can goes.

--- src/model/Queen.java
+++ src/model/Queen.java
@@ -15,6 +15,28 @@ public class Queen extends Piece {
 	
 	@Override
 	public ArrayList<Point> getPossiblePoints(){	
-		return null;
+		ArrayList<Point> possiblePointsToGo = new ArrayList<Point>();
+		int x = (int)getPosition().getX();
+		int y = (int)getPosition().getY();
+		
+		for(int i = 0; i < 8; i++){
+			if(i != y)
+				possiblePointsToGo.add(new Point(x, i));
+			if(i != x)
+				possiblePointsToGo.add(new Point(i, y));
+		}
+		
+		for(int i = 1; i < 8; i++){
+			if(x + i < 8 && y + i < 8)
+				possiblePointsToGo.add(new Point(x + i, y + i));
+			if(x - i >= 0 && y + i < 8)
+				possiblePointsToGo.add(new Point(x - i, y + i));
+			if(x + i < 8 && y - i >= 0)
+				possiblePointsToGo.add(new Point(x + i, y - i));
+			if(x - i >= 0 && y - i >= 0)
+				possiblePointsToGo.add(new Point(x - i, y - i));
+		}
+		
+		return possiblePointsToGo;
 	}
 }
